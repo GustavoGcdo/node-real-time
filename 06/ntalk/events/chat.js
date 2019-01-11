67ddb345a28eaf127935566e2e3eff84c9fc32de
Gustavo Candido de Oliveira <gustavo.gcdo@gmail.com>
2019-01-11 08:59:59 -0300

    Capitulo 6 - Notificações e status

--- 06/ntalk/events/chat.js
+++ 06/ntalk/events/chat.js
@@ -1,14 +1,21 @@
 module.exports = (app, io) => {
+  const onlines = {};
+
   io.on('connection', client => {
     const { session } = client.handshake;
     const { usuario } = session;
+
+    onlines[usuario.email] = usuario.email;
+    for (const email in onlines) {
+      client.emit('notify-onlines', email);
+      client.broadcast.emit('notity-onlines', email);
+    }
 
     client.on('send-server', (hashDaSala, msg) => {
       const novaMensagem = { email: usuario.email, sala: hashDaSala };
       const resposta = `<b>${usuario.nome}:</b> ${msg}<br>`;
       session.sala = hashDaSala;
 
-      // client.emit('send-client', resposta);
       client.broadcast.emit('new-message', novaMensagem);
       io.to(hashDaSala).emit('send-client', resposta);
     });
@@ -20,8 +27,12 @@ module.exports = (app, io) => {
 
     client.on('disconnect', () => {
       const { sala } = session;
+      const resposta = `<b>${usuario.nome}:</b>	saiu.<br>`;
+      delete onlines[usuario.email];
       session.sala = null;
       client.leave(sala);
+      client.broadcast.emit('notify-offlines', usuario.email);
+      io.to(sala).emit('send-client', resposta);
     });
   });
 };
